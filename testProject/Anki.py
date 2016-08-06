d5cf77e5208b1fddad3be602c7bdc8c710c0368c
LiangYanZhejiang <liangyan.zhejiang@163.com>
2016-08-06 21:33:13 +0800

    #1优化文件的格式

--- testProject/Anki.py
+++ testProject/Anki.py
@@ -12,6 +12,7 @@ words_path = os.getcwd() + '\\Words'
 Import_path = os.getcwd() + '\\Import_path'
 word_url = 'http://cn.bing.com/dict/search?q=%s'
 sentance_url = 'http://dict.youdao.com/example/mdia/audio/%s'
+bbc_url = 'http://dict.youdao.com/example/auth/%s'
 
 headers = [
     {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0'},
@@ -52,7 +53,7 @@ def do_post(url, dicid):
 
 def beautiful_content(means, tag):
     result = ''
-    for mean in means.descendants:
+    for mean in means.contents:
         if hasattr(mean, 'text'):
             result = result + mean.text
             if (mean.name == tag):
@@ -81,43 +82,43 @@ def do_request(word):
     
     #音标获取
     f = open(wordinfo_path + '\\' + 'pr_US.txt', 'w', encoding='utf-8')
-    us_pr = soup.find("div", class_="hd_prUS")
+    us_pr = soup.find('div', class_='hd_prUS')
     f.write(us_pr.text)
     f.close()
     f = open(wordinfo_path + '\\' + 'pr_En.txt', 'w', encoding='utf-8')
-    usa_pr = soup.find("div", class_="hd_pr")
+    usa_pr = soup.find('div', class_='hd_pr')
     f.write(usa_pr.text)
     f.close()
 
     #音标的读音获取
     mp3_us = True
-    for pronounce in soup.find_all("a",class_="bigaud"):
-        mouseover = pronounce.get("onmouseover")
+    for pronounce in soup.find_all('a',class_='bigaud'):
+        mouseover = pronounce.get('onmouseover')
         if mouseover == None:
             continue
-        i = mouseover.find("http")
-        j = mouseover.find("mp3")
+        i = mouseover.find('http')
+        j = mouseover.find('mp3')
         if i == -1 or j == -1:
             continue
         mp3link = mouseover[i:j+3]
         if (mp3_us):
-            mp3_path = wordinfo_path + "\\us.mp3"
+            mp3_path = wordinfo_path + '\\us.mp3'
         else:
-            mp3_path = wordinfo_path + "\\en.mp3"
+            mp3_path = wordinfo_path + '\\en.mp3'
         
         urllib.request.urlretrieve(mp3link, mp3_path)
         mp3_us = False
         
     #单词意思获取
-    word_mean = wordinfo_path + "\\mean.txt"
+    word_mean = wordinfo_path + '\\mean.txt'
     f = open(word_mean, 'w', encoding='utf-8')
-    means = soup.find("div", class_="qdef")
+    means = soup.find('div', class_='qdef')
     for mean in means.find_all('li'):
-        f.write(mean.text + "\n")
+        f.write('%s\t%s\n' %(mean.contents[0].text,mean.contents[1].text))
     f.close()
 
     #单词的补充
-    addition_path = wordinfo_path + "\\addition.txt"
+    addition_path = wordinfo_path + '\\addition.txt'
     addition_mean = means.find(class_='hd_if')
     if addition_mean != None:
         f = open(addition_path, 'w', encoding='utf-8')
@@ -153,8 +154,8 @@ def do_request(word):
                 str = str[x+2:y]                
             tagmap[str+'id']=atag.text
     
-    #f获取的是释义+例句
-    #f2仅获取的是释义
+    #f获取的是释义
+    #f2仅获取的是释义+例句
     tagmap_keys=list(tagmap.keys())
     for key in tagmap_keys:
         #网络的忽略之
@@ -163,41 +164,53 @@ def do_request(word):
             
         f = open(wordinfo_path + '\\' + tagmap[key] + '.txt', 'w', encoding = 'utf-8')
         mean = means.find(id=key)
-        sentence= mean.find(class_='li_sen')
+        sentences= mean.find(class_='li_sen')
         #权威英汉双解，英汉，英英
-        if (sentence != None):
+        if (sentences != None):
             f2 = open(wordinfo_path + '\\' + tagmap[key] + '_lis.txt', 'w', encoding = 'utf-8')
-            f2.write(sentence.text)
-            for no_lis in mean.find_all(class_='se_lis'):
-                f.write(no_lis.text)
+            for seg in sentences.find_all(class_='de_seg'):
+                for sentence in seg.contents:
+                    if 'dis' in sentence.get('class'):
+                        f2.write('%s\n' % sentence.text)
+                        f.write('%s\n' % sentence.text)
+                    elif 'se_lis' in sentence.get('class'):
+                        detail = sentence.contents[0].contents[0]
+                        str = '%s\t%s\n' %(detail.contents[0].text, detail.contents[1].text)
+                        f2.write(str)
+                        f.write(str)
+                    else:
+                        for detail in sentence.find_all(class_='li_ex'):
+                            f2.write('%s\n%s\n' %(detail.contents[0].text,detail.contents[1].text))
             f2.close()
         #搭配，同义，反义
         else:
-            f.write(mean.text)
+            for detail in mean.find_all(class_='df_div2'):
+                f.write('%s\t%s\n' %(detail.contents[0].text,detail.contents[1].text))
         f.close()
-        
+'''        
     #获取例句及读音
-#    senDefLink = soup.find("div", class_="senDefLink")
-#    if senDefLink == None:
-#        return    
-#    for aTag in senDefLink.find_all('a'):
-#        aTagid = aTag.get('id')
-#        if aTagid == 'sde_all':
-#            sentence_path = wordinfo_path + '\\all'
-#            do_savesentence(sentence_path, soup)
-#        else:
-#            aTagh = aTag.get('h')
-#            try:
-#                text = do_post(url, aTagh[3:])
-#            except socket.timeout as e:
-#                tips = "Exception happened:can't get Word--%s all sentances mp3 files." % word        
-#                print(tips)
-#                print(e)
-#                continue
-#            
-#            other_soup = BeautifulSoup(text, "html.parser")
-#            sentence_path = wordinfo_path + '\\' + aTagid[4:]
-#            do_savesentence(sentence_path, other_soup)
+    senDefLink = soup.find("div", class_="senDefLink")
+    if senDefLink == None:
+        return    
+    for aTag in senDefLink.find_all('a'):
+        aTagid = aTag.get('id')
+        if aTagid == 'sde_all':
+            sentence_path = wordinfo_path + '\\all'
+            do_savesentence(sentence_path, soup)
+        else:
+            aTagh = aTag.get('h')
+            try:
+                text = do_post(url, aTagh[3:])
+            except socket.timeout as e:
+                tips = "Exception happened:can't get Word--%s all sentances mp3 files." % word        
+                print(tips)
+                print(e)
+                continue
+            
+            other_soup = BeautifulSoup(text, "html.parser")
+            sentence_path = wordinfo_path + '\\' + aTagid[4:]
+            do_savesentence(sentence_path, other_soup)
+'''
 
     #原音获取
     sentence_url = sentance_url % word
@@ -225,7 +238,6 @@ def do_request(word):
             mp3_path = '%s\\%d.mp3' %(sentence_path , sentenceNo)
             urllib.request.urlretrieve(mp3link, mp3_path)            
         sentenceNo = sentenceNo + 1    
-
 
 if __name__ == "__main__":
     if not os.path.exists(words_path):
